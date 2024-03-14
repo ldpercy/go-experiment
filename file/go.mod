48e6282d8b924bd5b83f0edeba5609779508d189
Paul Dale <ldpercy@gmail.com>
2024-03-14 21:53:12 +1100

    file

---
+++ file/go.mod
@@ -0,0 +1,3 @@
+module experiment/file
+
+go 1.22.1
---
+++ file/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+
+	//read file
+	//echo file
+
+	filename := "test.txt"
+
+	// show file stats
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(file.Stat())
+
+	// read the file
+	content, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Err")
+	}
+	fmt.Println(string(content))
+
+	defer file.Close()
+}
